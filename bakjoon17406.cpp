46f42b10d5ea44d67e4ec8103bf22961ccacd28a
rullu <may05200@naver.com>
2020-05-31 19:34:18 +0900

    rotate array + brute force

--- bakjoon17406.cpp
+++ bakjoon17406.cpp
@@ -1,9 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <vector>
+#include <algorithm>
 
-int rotate(int r, int c, int s, int a[][50],int n, int m){
+using namespace std;
+
+typedef struct _Cor{
+	int r;
+	int c;
+	int s;
+}cor;
+
+cor info[6];
+vector <int> list(6);
+vector <int> visit(6);
+int init[50][50];
+int a[50][50]={0,};
+int n, m, k;
+int min_val=5000;
+
+void rotate(int, int, int);
+int calculate();
+void initArr();
+void dfs(int);
+
+
+void rotate(int r, int c, int s){ //rotate and calculate min value
 	int result=5000;
-	int i,j,k;
 	int l_x=r-s-1;
 	int l_y=c-s-1;
 	int r_x=r+s-1;
@@ -11,60 +34,97 @@ int rotate(int r, int c, int s, int a[][50],int n, int m){
 	int temp;
 	int copy[50][50];
 	
-	for(i=0;i<n;i++){
-		for(j=0;j<m;j++)
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++)
 			copy[i][j]=a[i][j];
 	}
 	
-	k=0;
-	for(i=r_x-l_x;i>0;i=i-2){	
-		for(j=0;j<i;j++){
+	int k=0;
+	for(int i=r_x-l_x;i>0;i-=2){	
+		for(int j=0;j<i;j++){
 			a[l_x+k][l_y+k+j+1]=copy[l_x+k][l_y+k+j];
 		}
-		for(j=0;j<i;j++){
+		for(int j=0;j<i;j++){
 			a[l_x+k+j+1][r_y-k]=copy[l_x+k+j][r_y-k];
 		}
-		for(j=0;j<i;j++){
+		for(int j=0;j<i;j++){
 			a[r_x-k][r_y-k-j-1]=copy[r_x-k][r_y-k-j];
 		}
-		for(j=0;j<i;j++){
+		for(int j=0;j<i;j++){
 			a[r_x-k-j-1][l_y+k]=copy[r_x-k-j][l_y+k];
 		}
 		k++;
 	}
 	
-	for(i=0;i<n;i++){
+}
+
+int calculate(){
+	int temp;
+	int result=5000;
+	for(int i=0;i<n;i++){
 		temp=0;
-		for(j=0;j<m;j++){
-			temp=a[i][j]+temp;
+		for(int j=0;j<m;j++){
+			temp+=a[i][j];
 		}
 		if(temp<result)
 			result=temp;
 	}
+	
 	return result;
 }
 
+void initArr(){
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++)
+			a[i][j]=init[i][j];
+	}
+}
+
+void dfs(int depth){
+	
+	if(depth==k){
+		initArr();
+		for(int i=0;i<k;i++){
+			int idx=list[i];
+			rotate(info[idx].r, info[idx].c, info[idx].s);
+		}
+		
+		int temp=calculate();
+		if(temp<min_val){
+			min_val=temp;
+		}
+		return;
+	}
+	
+	for(int i=0;i<k;i++){
+		if(visit[i]==0){
+			visit[i]=1;
+			list[depth]=i;
+			dfs(depth+1);
+			visit[i]=0;
+		}
+	}
+}
+
 int main(){
-	int n,m,k;
-	int a[50][50]={0,};
 	int r,s,c;
-	int i,j;
-	int min=5000;
-	int temp;
 	
 	scanf("%d %d %d",&n,&m,&k);
-	for(i=0;i<n;i++){
-		for(j=0;j<m;j++)
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
 			scanf("%d",&a[i][j]);
+			init[i][j]=a[i][j];
+		}
 	}
 	
-	for(i=0;i<k;i++){
+	for(int i=0;i<k;i++){
 		scanf("%d %d %d",&r,&c,&s);
-		temp=rotate(r,c,s,a,n,m);
-		
-		if(min>temp)
-			min=temp;
+		info[i].r=r;
+		info[i].c=c;
+		info[i].s=s;
 	}
 	
-	printf("%d\n",min);
+	dfs(0);
+	
+	printf("%d\n",min_val);
 }
