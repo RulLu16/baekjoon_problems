e579dc1303ba060ab0fa3a607aa185146c59e350
RulLu16 <may05200@naver.com>
2020-08-14 21:48:12 +0900

    max-heap

---
+++ 11279.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+	int n;
+	priority_queue<int, vector<int>, less<int> > que;
+	
+	scanf("%d",&n);
+	
+	for(int i=0;i<n;i++){
+		int x;
+		scanf("%d",&x);
+		
+		if(x==0){
+			if(que.empty()){
+				printf("0\n");
+			}
+			else{
+				printf("%d\n",que.top());
+				que.pop();
+			}
+		}
+		else{
+			que.push(x);
+		}
+	}
+}
